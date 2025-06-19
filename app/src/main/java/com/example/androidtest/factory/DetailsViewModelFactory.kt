59f425c110b3ff0cd5a57f26b68eafb528f99854
Djakhar <djakhar.khadjiev@etu.unistra.fr>
2025-06-19 20:38:07 +0200

    ajout du detailsviewmodelFactory

---
+++ app/src/main/java/com/example/androidtest/factory/DetailsViewModelFactory.kt
@@ -0,0 +1,17 @@
+package com.example.androidtest.factory
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.androidtest.data.ContactRepository
+import com.example.androidtest.viewmodel.DetailsViewModel
+
+class DetailsViewModelFactory(private val repository: ContactRepository): ViewModelProvider.Factory {
+    override fun <T : ViewModel>create(modelClass: Class<T>):T{
+        if (modelClass.isAssignableFrom(DetailsViewModel::class.java))
+        {
+            @Suppress("UNCHECKED_CAST")
+            return DetailsViewModel(repository) as T
+        }
+        throw IllegalArgumentException("UNKNOW view model")
+    }
+}
